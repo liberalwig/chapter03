181f9a3f38a4fdee087fe7757bd573d569fc0e41
최승은 <liberalfilm@gmail.com>
2021-12-10 08:14:27 +0900

    2021.12.10(금)08:13 Object클래스 메소드 중 equals()[객체동등비교]

--- src/com/javaex/ex04/Rectangle.java
+++ src/com/javaex/ex04/Rectangle.java
@@ -1,49 +1,54 @@
+//2021.12.10(금)08:13 복습 후,
+
 package com.javaex.ex04;
 
 public class Rectangle {
 
-	//필드
+	// 필드
 	private int width;
 	private int height;
-	
-	//생성자
+
+	// 생성자
 	public Rectangle() {
 		super();
 	}
+
 	public Rectangle(int width, int height) {
 		super();
 		this.width = width;
 		this.height = height;
 	}
 
-	//메소드 gs
+	// 메소드 gs
 	public int getWidth() {
 		return width;
 	}
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
+
 	public int getHeight() {
 		return height;
 	}
+
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
-	//메소드 일반
+
+	// 메소드 일반
 	@Override
 	public String toString() {
 		return "Rectangle [width=" + width + ", height=" + height + "]";
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		if(this.width == ((Rectangle)obj).width && this.height == ((Rectangle)obj).height) {
+		if (this.width == ((Rectangle) obj).width && this.height == ((Rectangle) obj).height) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	
+
 }
--- src/com/javaex/ex04/RectangleApp.java
+++ src/com/javaex/ex04/RectangleApp.java
@@ -1,15 +1,17 @@
+//2021.12.10(금)08:13 복습 후,
+
 package com.javaex.ex04;
 
 public class RectangleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Rectangle a = new Rectangle(6,4);
-		Rectangle b = new Rectangle(6,4);
-		Rectangle c = new Rectangle(12,2);
-		
-		System.out.println("===euals()값 비교");
+
+		Rectangle a = new Rectangle(6, 4);
+		Rectangle b = new Rectangle(6, 4);
+		Rectangle c = new Rectangle(12, 2);
+
+		System.out.println("===equals()값 비교");
 		System.out.println(a.equals(b));
 		System.out.println(a.equals(c));
 	}
