842a6a8f7bf8a69c46a7df4315c80b4825f1111d
최승은 <liberalfilm@gmail.com>
2021-12-13 10:34:31 +0900

    2021.12.13(월)10:18 equals(), Object DownCasting

--- src/com/javaex/ex03/Point.java
+++ src/com/javaex/ex03/Point.java
@@ -1,6 +1,10 @@
+/*2021.12.2(목)오후 대면수업
+멍해서 들어도 이해 안 했고
+2021.12.12(일)17:21, 2021.12.13(월)10:04에 영상 복습(사실상 최초)*/
+
 package com.javaex.ex03;
 
-public class Point{
+public class Point {
 
 	// 필드
 	private int x;
@@ -39,25 +43,23 @@ public class Point{
 		return "Point [x=" + x + ", y=" + y + "]";
 	}
 
-	
-	@Override
-	public boolean equals(Object obj) {
-		Point p =(Point)obj;   //Object 다운캐스팅
-		// a.x==b.x &&  a.y==b.y
+	@Override // x끼리 같고 y끼리 같고 둘은 동시에 충족해야 함
+	public boolean equals(Object obj) {// object로 섞어썼기 때문에 obj.x하면 자식 가려져셔 모르니 obj를 강제캐스팅
 		
-		/*
-		System.out.println("a.x=" + this.x);
-		System.out.println("a.y=" + this.y);
+
+		/*이건 새로 Point 속 p라고 써준 경우이고 밑은
+		Point p = (Point) obj;
+		if (this.x == p.x && this.y == p.y) {
+		 */
 		
-		System.out.println("b.x=" + p.x);
-		System.out.println("b.y=" + p.y);
-		*/
-		
-		if(this.x==p.x  && this.y==p.y) {
+		if (this.x == (Point)obj).x && this.y == ((Point)obj).y) {
 			return true;
-		}else {
+
+		} else {
 			return false;
 		}
 	}
-	
+
 }
+
+//자식클래스로 Point가 올 줄 몰랐기에 Object로 씀
--- src/com/javaex/ex03/PointApp.java
+++ src/com/javaex/ex03/PointApp.java
@@ -1,23 +1,26 @@
+/*2021.12.2(목)오후 대면수업
+멍해서 들어도 이해 안 했고 2021.12.12(일)17:21, 2021.12.13(월)10:04에 영상 복습(사실상 최초)
+: Object의 toString*/
+
 package com.javaex.ex03;
 
 public class PointApp {
 
 	public static void main(String[] args) {
-		
-		Point a = new Point(2,3);
-		Point b = new Point(2,3);
+
+		Point a = new Point(2, 3);
+		Point b = new Point(2, 3);
 		Point c = new Point(100, 139);
-		
+
 		Point d = c;
-		
-		System.out.println(a==b);
-		System.out.println(d==c);
+
+		System.out.println(a == b);
+		System.out.println(d == c);
 
 		System.out.println("=======================");
 		System.out.println(a.equals(b));
 		System.out.println(b.equals(c));
-		
-		
+
 	}
 
 }
